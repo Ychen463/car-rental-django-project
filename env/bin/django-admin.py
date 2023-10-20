4269c7d405837b66e39dfb4165436a70da408fd9
yanlinchen <yc3156@nyu.edu>
2023-10-20 10:09:38 -0400

    first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/yanlinchen/Documents/NYU/Portfolio/car-rental-project/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
