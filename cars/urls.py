390aa840813fa59df33ca1d87372f2e1f5584c1c
ychen463 <gisellec2@163.com>
2023-10-22 01:05:25 -0400

    Completed search funtionality, admin customization

--- cars/urls.py
+++ cars/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.cars, name='cars'),
-    # path('<int:id>', views.car_detail, name='car_detail'),
-    # path('search', views.search, name='search'),
+    path('<int:id>', views.car_detail, name='car_detail'),
+    path('search', views.search, name='search'),
     # ... your other URL patterns ...
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
